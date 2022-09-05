c593cef022539c3cb818d60c5a05eab4976cc51d
Zheng Zhang <zheng_zhang1@alumni.brown.edu>
2022-09-05 22:32:44 +0200

    Printing message set up

--- main.py
+++ main.py
@@ -13,12 +13,11 @@ t2 = time.time()
 print("FoM is {}!\n".format(figure_of_merit))
 print("Elapsed time {}".format(t2-t1))
 
+
 def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 ⌘F8 切换断点。
+    print(f'Hi, {name}')
 
 
-# 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
     print_hi('there! \n')
 
