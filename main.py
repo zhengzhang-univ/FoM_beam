fe8b6cdedb93c9433fa8af7d98a2b0d5bb505dce
Zheng Zhang <zheng_zhang1@alumni.brown.edu>
2022-09-07 11:22:32 +0200

    Printing message set up

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import mpiutil
 from FoM import FoM
 import time
+import numpy as np
 
 t1 = time.time()
 # Read beam files
@@ -8,13 +9,14 @@ beam_file_path = '../'
 
 test = FoM(400, 800, 201, beam_file_path)
 
-figure_of_merit_1 = test.FoM(if_1st_order = True)
-figure_of_merit = test.FoM(if_1st_order = False)
+figure_of_merit_1 = test.FoM(if_1st_order=True)
+figure_of_merit = test.FoM(if_1st_order=False)
 
 t2 = time.time()
 if mpiutil.rank0:
     print("FoM with the first order term is\n {}!\n".format(figure_of_merit_1))
     print("FoM w/o the first order term is\n {}!\n".format(figure_of_merit))
+    print("FoM w/o the first order term is {} in total!\n".format(np.sum(figure_of_merit)))
     print("Elapsed time {}".format(t2-t1))
 
 
